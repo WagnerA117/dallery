7ebcdf25cdfbadd71095151454de7bc11bf2959c
Ahren <ahren@meetingpal.co.za>
2023-03-24 08:19:56 +0200

    Add gallery with open and close

--- app/firebase/clientApp.ts
+++ app/firebase/clientApp.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const clientCredentials = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const clientCredentials = {
 const app = !getApps().length ? initializeApp(clientCredentials) : getApp();
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const auth = getAuth();
 export const gitHubProvider = new GithubAuthProvider();
