96969744a360625aa362e90172659626257f35e9
Ahren <ahren@meetingpal.co.za>
2023-04-26 23:12:00 +0200

    this is some git stuff

--- app/pages/galleries/[gallery]/page.tsx
+++ app/pages/galleries/[gallery]/page.tsx
@@ -4,7 +4,15 @@ import LoadingSpinner from "@/app/components/higherOrderComponent/LoadingSpinner
 import { db } from "@/app/firebase/clientApp ";
 import { GalleryType, ImageType } from "@/app/firebase/types ";
 import getFirebaseGallery from "@/app/functions/FirebaseFunctions/getFirebaseGallery ";
-import { Box, Button, Flex, Image, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import { arrayUnion, doc } from "firebase/firestore";
 import { updateDoc } from "firebase/firestore";
 import {
@@ -14,7 +22,8 @@ import {
   uploadString,
 } from "firebase/storage";
 import { useSearchParams } from "next/navigation";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import { v4 } from "uuid";
 
 import { storage } from "../../../firebase/clientApp";
@@ -25,6 +34,15 @@ interface Image {
 }
 type FileListArray = Array<FileList | null>;
 
+type FileType = {
+  path: string;
+  preview: string;
+  lastModified: number;
+  name: string;
+  size: number;
+  type: string;
+};
+
 const Gallery: React.FC = () => {
   const searchParams = useSearchParams();
 
@@ -32,16 +50,12 @@ const Gallery: React.FC = () => {
 
   const [loading, setLoading] = useState(true);
   const [gallery, setGallery] = useState<GalleryType>();
-  const [selectedFiles, setSelectedFiles] = useState<[] | null>([]);
-  const [dataUrls, setDataUrls] = useState<[] | null>([]);
-  const [uploadFiles, setUploadFiles] = useState<[] | null>([]);
+  const [files, setFiles] = useState<FileType[] | []>([]);
+  const [rejected, setRejected] = useState([]);
 
-  const selectedFileRef = useRef<HTMLInputElement>(null);
+  //  const selectedFileRef = useRef<HTMLInputElement>(null);
 
   //  const { gallery, loading, error } = useGallery(galleryId!);
-  if (!galleryId) {
-    return <Box> no gallery id </Box>;
-  }
 
   useEffect(() => {
     const getGallery = async () => {
@@ -56,32 +70,41 @@ const Gallery: React.FC = () => {
     getGallery();
   }, []);
 
+  const onDrop = useCallback(
+    (acceptedFiles: FileType, rejectedFiles: FileType) => {
+      console.log(acceptedFiles);
+      if (acceptedFiles) {
+        setFiles((previousFiles) => [
+          ...previousFiles,
+          ...acceptedFiles.map((file) =>
+            Object.assign(file, { preview: URL.createObjectURL(file) })
+          ),
+        ]);
+      }
+
+      if (rejectedFiles?.length) {
+        setRejected((previousFiles) => [...previousFiles, ...rejectedFiles]);
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    // Revoke the data uris to avoid memory leaks
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+  const removeFile = (name: string) => {
+    setFiles((files) => files?.filter((file) => file?.name !== name));
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   if (loading === true) {
     return <LoadingSpinner />;
   }
 
   //This handles uploading the files to manage before saving to firebase.
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //Now to try get this image with name properties.
-
-    if (event.target.files) {
-      const newFiles = Array.from(event.target.files);
-      setSelectedFiles((prevFiles) => [...prevFiles, ...newFiles]);
-
-      newFiles.map((file) => {
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-          if (e.target?.result) {
-            setSelectedFiles((prevFiles) => [...prevFiles, e.target.result]);
-          }
-        };
-
-        reader.readAsDataURL(file);
-      });
-    }
-  };
 
   const saveSelected = async () => {
     //save selected files to firebase
@@ -118,55 +141,56 @@ const Gallery: React.FC = () => {
     }
   };
 
-  console.log(selectedFiles, " this is the selected files");
-  console.log(dataUrls, " this is the data urls");
-  console.log(uploadFiles, " this is the upload files");
-
   return (
     <>
       <Flex justify="center" align="center" width="100%">
         <Flex>
-          {selectedFiles ? (
-            <Button
-              onClick={() => {
-                setSelectedFiles([]);
-                setDataUrls([]);
-              }}
-              bg="orange.500"
-            >
-              {" "}
-              Cancel
-            </Button>
-          ) : null}
-          <Button
+          {/*<Button
             bg="starNight.medium"
             onClick={() => {
               selectedFileRef.current?.click();
             }}
           >
             Select Images
-          </Button>
+          </Button>*/}
 
           <Button onClick={saveSelected}> Save Images</Button>
         </Flex>
       </Flex>
 
-      <Input
-        ref={selectedFileRef}
-        type="file"
-        hidden
-        accept="image/x-png,image/gif,image/jpeg"
-        multiple
-        onChange={handleFileChange}
-      ></Input>
+      <Flex border="8px" minHeight="100px">
+        <Box {...getRootProps()} minHeight="">
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <Box>Drag 'n' drop some files here, or click to select files</Box>
+          )}
+        </Box>
+      </Flex>
 
-      {selectedFiles?.map((file) => {
-        return (
-          <Box>
-            <Image src={file[1]} key={Math.random()} />
-          </Box>
-        );
-      })}
+      <Flex justify="center" alignItems="center">
+        {files ? (
+          <Button
+            onClick={() => {
+              setFiles([]);
+            }}
+            bg="orange.500"
+          >
+            {" "}
+            Cancel
+          </Button>
+        ) : null}
+
+        <List>
+          {files?.map((item) => (
+            <ListItem>
+              <Image src={item?.preview} />
+              <Button onClick={() => removeFile(item?.name)}> Remove</Button>
+            </ListItem>
+          ))}
+        </List>
+      </Flex>
     </>
   );
 };
