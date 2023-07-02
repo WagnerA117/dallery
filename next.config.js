879275b731c23431a88427f7754ad3c3cb4c8270
Ahren <ahren@meetingpal.co.za>
2023-07-02 12:04:17 +0200

    build configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
